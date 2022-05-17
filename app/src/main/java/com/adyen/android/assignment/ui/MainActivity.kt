2941a025dbf1f6b6f68b1a2c58ffb2eb3223fd9b
Zainab Jimoh <jimohzainab4753@gmail.com>
2022-05-17 12:12:54 +0100

    Set up navigation and fragments

--- app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
@@ -1,7 +1,50 @@
 package com.adyen.android.assignment.ui
 
+import android.content.res.Configuration
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.adyen.android.assignment.R
+import com.adyen.android.assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    // TODO: Placeholder activity that may be used
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
+    private lateinit var appBarConfig: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.placesListFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfig)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
 }
---
+++ app/src/main/java/com/adyen/android/assignment/ui/places/PlacesListViewModel.kt
@@ -0,0 +1,7 @@
+package com.adyen.android.assignment.ui.places
+
+import androidx.lifecycle.ViewModel
+
+class PlacesListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
